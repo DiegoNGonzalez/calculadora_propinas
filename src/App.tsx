78a5a8e745783ef17bddf177386548bb03452452
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-10-20 14:26:01 -0300

    agregando tailwind css y limpiando archivos

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className="text-9xl font-black bg-teal-500"  >Hola mundo</h1>
+     <p className="text-lg text-indigo-400">Hola mundo 2</p>
+    </>
+  )
+}
+
+export default App
